50df3aad5be95e6ac8cea9b63910db925e122fa0
Matt Woods <mattwoods21@gmail.com>
2017-09-13 18:55:14 -0500

    added getInput method to UI Controller

--- app.js
+++ app.js
@@ -1,33 +1,62 @@
+	// BUDGET CONTROLLER
 var budgetController = (function() {
 	
-	var x = 23;
 
-	var add = function(a) {
-		return x + a;
-	}
-
-	return {
-		publicTest: function(b) {
-			return add(b);
-		}
-}
 
 })();
 
+
+	// UI CONTROLLER
 var UIController = (function() {
 
-
-
-})();
-
-var controller = (function(budgetCtrl, UICtrl) {
-
-	var z = budgetCtrl.publicTest(5);
-
 	return {
-		anotherPublic: function() {
-			console.log(z);
+		getinput: function() {
+			return {
+				type: document.querySelector('.add__type').value, //will be either inc or exp
+				description: document.querySelector('.add__description').value,
+				value: document.querySelector('.add__value').value
+			};
 		}
 	}
 
+})();
+
+	// GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl) {
+
+	var ctrlAddItem = function() {
+
+		// 1. Get the field input data
+		var input = UICtrl.getinput();
+		console.log(input);
+
+		// 2. Add the item to the budget controller
+
+		// 3. Add the item to UI
+
+		// 4. Calculate the budget
+
+		// 5. Display the budget on the UI
+		
+	}
+
+	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+	document.addEventListener('keypress', function(event){
+
+		if(event.keyCode === 13 || event.which === 13) {
+			ctrlAddItem();
+		}
+
+	});
+
+
 })(budgetController, UIController);
+
+
+
+
+
+
+
+
